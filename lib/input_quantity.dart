18258c4a5beca0d51f473d03ba1aa107c8170c34
Modi <10958931+msxenon@users.noreply.github.com>
2024-08-05 20:52:46 +0200

    feat: Add InputQtyProfiler for number formatting

--- lib/input_quantity.dart
+++ lib/input_quantity.dart
@@ -2,3 +2,4 @@ export 'package:input_quantity/src/input_qty.dart';
 export 'package:input_quantity/src/constant.dart';
 export 'package:input_quantity/src/decoration_props.dart';
 export 'package:input_quantity/src/form_props.dart';
+export 'package:input_quantity/src/input_qty_profiler.dart';
